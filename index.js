a4504a42d356bd2b96af134093f79c3e61db779b
Lucas374405 <lucas374405@gmail.com>
2026-05-31 19:09:38 -0300

    Correção das rotas e exibição da lista de filmes

--- index.js
+++ index.js
@@ -6,20 +6,37 @@ app.engine('handlebars', exphbs.engine({ defaultLayout: false }));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.render('home', {
-    titulo: 'Atividade',
-    descricao: 'Precisa de descrição?'
-  });
+    res.render('home', {
+        titulo: 'Atividade',
+        BemVindo: 'Bem-vindo ao sistema',
+    });
+});
+
+app.get('/perfil', (req, res) => {
+    res.render('home', {
+        Nome: 'Lucas e Vinicius',
+        Idade: 20
+    });
+});
+
+app.get('/Lista-de-Filmes', (req, res) => {
+    const filmes = [
+        { titulo: 'One Piece Film Red', ano: 2022 },
+        { titulo: 'Mandaloriano e Grogu', ano: 2026 },
+        { titulo: 'Star Wars: Rogue Squadron', ano: 2023 }
+    ];
+    res.render('filmes', {
+        filmes: filmes    
+    });
 });
 
 app.listen(3000, () => {
   console.log('Servidor funcionando');
 });
 
-app.get('/', (req, res) => {
-    res.send('Bem-vindo ao sistema');
-});
-
+//app.get('/', (req, res) => {
+   // res.send('Bem-vindo ao sistema');
+//});
 
 app.get('/sobre', (req, res) => {
     res.send('sobre');
@@ -28,7 +45,7 @@ app.get('/sobre', (req, res) => {
 app.get('/contato', (req, res) => {
     res.json({
         'email': 'Aluno@discente.ifpe.edu.br',
-        'telefone': '81 99999-9999'
+        'telefone': '747324556'
     });
 });
 
@@ -37,33 +54,37 @@ app.get('/erro', (req, res) => {
         'mensagem': 'Erro: Página não encontrada'
     });
 });
-    
-app.get ('/inicio', (req, res) => {
+
+app.get('/inicio', (req, res) => {
     res.redirect('/');
 });
 
-app.get ('/Usuario/:id', (req, res) => {
+app.get('/Usuario/:id', (req, res) => {
     res.send(`Usuario: ${req.params.id}`);
 });
 
-app.get ('/produto/:nome', (req, res) => {
+app.get('/produto/:nome', (req, res) => {
     res.send(`Produto: ${req.params.nome}`);
 });
 
-app.get ('/filme/:id/:nome', (req, res) => {
-    res.send (`
+app.get('/filme/:id/:nome', (req, res) => {
+    res.send(`
         Filme: ${req.params.nome} <br>
         ID: ${req.params.id}
-    `)
+    `);
 });
 
-app.get ('/buscar', (req, res) => {
-    res.send (`buscar: ${req.query.nome}`);
+app.get('/buscar', (req, res) => {
+    res.send(`buscar: ${req.query.nome}`);
 });
 
-app.get ('/produto', (req, res) => {
-    res.send (`
+app.get('/produto', (req, res) => {
+    res.send(`
        Categoria: ${req.query.categoria} <br>
        Página: ${req.query.pagina} 
-        `);
+    `);
+});
+
+app.get('/usuario', (req, res) => {
+    res.send(`Filtrando usuario por idade: ${req.query.idade}`);
 });
